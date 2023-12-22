11ec74cf819def6995a6b1ebddcb9baa893b0188
Mcdemzy <adeyemoademola12@gmail.com>
2023-12-22 22:41:38 +0100

    Completed Product Page

--- screens/ProductDetails.jsx
+++ screens/ProductDetails.jsx
@@ -1,14 +1,109 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Text, View, Image } from "react-native";
+import React, { useState } from "react";
+import {
+  Ionicons,
+  SimpleLineIcons,
+  MaterialCommunityIcons,
+  Fontisto,
+} from "@expo/vector-icons";
+import styles from "./productDetails.style";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { COLORS, SIZES } from "../constants";
 
-const ProductDetails = () => {
+const ProductDetails = ({ navigation }) => {
+  const [count, setCount] = useState(1);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrement = () => {
+    if (count > 1) {
+      setCount(count - 1);
+    }
+  };
   return (
-    <View>
-      <Text>ProductDetails</Text>
+    <View style={styles.container}>
+      <View style={styles.upperRow}>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.goBack();
+          }}
+        >
+          <Ionicons name="chevron-back-circle" size={30} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => {}}>
+          <Ionicons name="heart" size={30} color={COLORS.primary} />
+        </TouchableOpacity>
+      </View>
+
+      <Image
+        source={{
+          uri: "https://images.pexels.com/photos/1571460/pexels-photo-1571460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        }}
+        style={styles.image}
+      />
+
+      <View style={styles.details}>
+        <View style={styles.titleRow}>
+          <Text style={styles.title}>Product</Text>
+          <View style={styles.priceWrapper}>
+            <Text style={styles.price}>$699.99</Text>
+          </View>
+        </View>
+        <View style={styles.ratingRow}>
+          <View style={styles.rating}>
+            {[1, 2, 3, 4, 5].map((index) => (
+              <Ionicons key={index} name="star" size={24} color="gold" />
+            ))}
+            <Text style={styles.ratingText}> (4.9)</Text>
+          </View>
+
+          <View style={styles.rating}>
+            <TouchableOpacity onPress={() => increment()}>
+              <SimpleLineIcons name="plus" size={20} />
+            </TouchableOpacity>
+            <Text style={styles.ratingText}> {count} </Text>
+            <TouchableOpacity onPress={() => decrement()}>
+              <SimpleLineIcons name="minus" size={20} />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={styles.descriptionWrapper}>
+          <Text style={styles.description}>Description</Text>
+          <Text style={styles.descText}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+            enim possimus quaerat quibusdam quis, placeat nemo voluptates! Cum,
+            ipsa repellat aspernatur quaerat, accusantium ad modi doloribus
+            incidunt ut eaque harum nam perferendis distinctio, quibusdam
+            veritatis eum. Ex quidem repudiandae consequuntur exercitationem
+            distinctio aliquam illo, quas possimus architecto amet perferendis
+            facilis tempore! Laboriosam iure quidem soluta.
+          </Text>
+        </View>
+        <View style={{ marginBottom: SIZES.small }}>
+          <View style={styles.location}>
+            <View style={{ flexDirection: "row" }}>
+              <Ionicons name="location-outline" size={20} />
+              <Text> Ibadan </Text>
+            </View>
+            <View style={{ flexDirection: "row" }}>
+              <MaterialCommunityIcons name="truck-delivery-outline" size={20} />
+              <Text> Free Delivery </Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.cartRow}>
+          <TouchableOpacity onPress={() => {}} style={styles.cartBtn}>
+            <Text style={styles.cartTitle}>Buy Now</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => {}} style={styles.addCart}>
+            <Fontisto name="shopping-bag" size={22} color={COLORS.lightWhite} />
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
 
 export default ProductDetails;
-
-const styles = StyleSheet.create({});
